738386242cf76dcb9007331a9588655108faab7b
ThisisKUNMENG <85155366+ThisisKUNMENG@users.noreply.github.com>
2022-01-20 17:32:08 +0800

    Add files via upload

---
+++ dorm_electricity.py
@@ -0,0 +1,18 @@
+import requests
+import json
+#from bs4 import BeautifulSoup
+import login
+
+class Elec(login.Fudan):
+    def get_dorm_electricity(self):
+        get_info = self.session.get(
+            'https://zlapp.fudan.edu.cn/fudanelec/wap/default/info')
+        if get_info.status_code == 200:
+            info = json.loads(get_info.text)
+            remain_elec = str(info['d']['fj_surplus'])
+        else:
+            remain_elec = 'failed to get'
+        return remain_elec
+
+uid, psw = login.get_account()
+
