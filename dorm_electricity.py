acc2e1181c6a7191d518e684f44350371472350a
ThisisKUNMENG <jiangyikun9663@gmail.com>
2022-01-26 16:06:05 +0800

    Update dorm_electricity.py

--- dorm_electricity.py
+++ dorm_electricity.py
@@ -10,10 +10,13 @@ class Elec(login.Fudan):
         get_info = self.session.get(
             'https://zlapp.fudan.edu.cn/fudanelec/wap/default/info')
         if get_info.status_code == 200:
-            info = json.loads(get_info.text)
-            remain_elec = str(info['d']['fj_surplus'])
+            try:
+                info = json.loads(get_info.text)
+                remain_elec = str(info['d']['fj_surplus'])
+            except Exception as e:
+                remain_elec = "failed to get dorm electricity with exception" + str(e)
         else:
-            remain_elec = 'failed to get'
+            remain_elec = 'failed to get drom electricity with status code' + str(get_info.status_code)
         return remain_elec
 
 uid, psw = login.get_account()
